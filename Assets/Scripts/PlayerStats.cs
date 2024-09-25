a83a8ef91f828c0ba5ac1958da8c7d59e8bafbf2
Charlie <charlieryancurry@gmail.com>
2024-09-25 09:49:44 -0300

    add Upgrade class

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,23 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour
+[Serializable]
+public class PlayerStats
 {
     //this will be where all the stats for the player are stored and will be called to use and will be affected by the upgrades.
 
     public int damage; // affects the damage
     public int attackSpeed; // affects the cooldowns
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/Upgrade.cs
@@ -0,0 +1,11 @@
+using System;
+
+[Serializable]
+public class Upgrade
+{
+    public string upgradeName;
+    public int cost;
+    public bool isPurchased;
+    // stat gained should be stored in here somewhere somehow
+    // we want the stat to be increased in here ?
+}

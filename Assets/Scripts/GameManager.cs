6e90d898da7c3620117ec4a185dee14aa57cb9da
Charlie <charlieryancurry@gmail.com>
2024-09-26 11:04:43 -0300

    add game manager and ui manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,177 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private UIManager uiManager;
+    private PlayerMovement player;
+    public GameObject playerSprite;
+    public GameObject spawnPoint;
+
+    public enum GameState
+    {
+        MainMenu,
+        Gameplay,
+        RunEnd,
+        Pause,
+        Credits,
+        Upgrades,
+        RunWin,
+        Settings
+    }
+
+    public GameState previousGameState;
+    public GameState gameState;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiManager = FindObjectOfType<UIManager>();
+        player = FindObjectOfType<PlayerMovement>();
+        gameState = GameState.MainMenu;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+
+        switch (gameState)
+        {
+            case GameState.MainMenu:
+                MainMenu();
+                break;
+            case GameState.Gameplay:
+                Gameplay();
+                break;
+            case GameState.Pause:
+                Pause();
+                break;
+            case GameState.Settings:
+                Settings();
+                break;
+            case GameState.RunEnd:
+                RunEnd();
+                break;
+            case GameState.Credits:
+                Credits();
+                break;
+            case GameState.Upgrades:
+                Upgrades();
+                break;
+            case GameState.RunWin:
+                RunWin();
+                break;
+        }
+    }
+
+    private void MainMenu()
+    {
+        Cursor.visible = true;
+        playerSprite.SetActive(false);
+        uiManager.UIMainMenu();
+    }
+
+    private void Gameplay()
+    {
+        Cursor.visible = true;
+        playerSprite.SetActive(true);
+        //lastPlayerPosition = player.transform.position;
+        uiManager.UIGameplay();
+    }
+
+    private void Settings()
+    {
+        Cursor.visible = true;
+        playerSprite.SetActive(false);
+        uiManager.UISettings();
+    }
+
+    private void Pause()
+    {
+        Cursor.visible = true;
+        playerSprite.SetActive(false);
+        uiManager.UIPause();
+    }
+
+    private void RunEnd()
+    {
+        Cursor.visible = true;
+        playerSprite.SetActive(false);
+        uiManager.UIRunEnd();
+    }
+
+    private void Credits()
+    {
+        Cursor.visible = true;
+        playerSprite.SetActive(false);
+        uiManager.UICredits();
+    }
+
+    private void Upgrades()
+    {
+        Cursor.visible = true;
+        playerSprite.SetActive(false);
+        uiManager.UIUpgrades();
+    }
+
+    private void RunWin()
+    {
+        Cursor.visible = true;
+        playerSprite.SetActive(false);
+        uiManager.UIRunWin();
+    }
+
+    public void PauseGame()
+    {
+        if (gameState != GameState.Pause && gameState == GameState.Gameplay)
+        {
+            previousGameState = gameState;
+            gameState = GameState.Pause;
+            Time.timeScale = 0;
+        }
+        else if (gameState == GameState.Pause || gameState == GameState.Settings)
+        {
+            gameState = previousGameState;
+            Time.timeScale = 1;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenSettings()
+    {
+        if (gameState != GameState.Pause)
+        {
+            previousGameState = gameState;
+        }
+        gameState = GameState.Settings;
+    }
+
+    public void OpenCredits()
+    {
+        gameState = GameState.Credits;
+    }
+
+    public void MovePlayerToSpawnPoint()
+    {
+        player.transform.position = spawnPoint.transform.position;
+    }
+
+    public void Back()
+    {
+        if (previousGameState != GameState.MainMenu)
+        {
+            gameState = GameState.Pause;
+        }
+        else
+        {
+            gameState = previousGameState;
+            Time.timeScale = 1;
+        }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,15 +4,109 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject MainMenuUI;
+    public GameObject PauseUI;
+    public GameObject GameplayUI;
+    public GameObject SettingsUI;
+    public GameObject RunEndUI;
+    public GameObject CreditsUI;
+    public GameObject UpgradesUI;
+    public GameObject RunWinUI;
+
+    public void UIMainMenu()
     {
-        
+        MainMenuUI.SetActive(true);
+        PauseUI.SetActive(false);
+        GameplayUI.SetActive(false);
+        SettingsUI.SetActive(false);
+        RunWinUI.SetActive(false);
+        CreditsUI.SetActive(false);
+        UpgradesUI.SetActive(false);
+        RunEndUI.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UIPause()
     {
-        
+        MainMenuUI.SetActive(false);
+        PauseUI.SetActive(true);
+        GameplayUI.SetActive(false);
+        SettingsUI.SetActive(false);
+        CreditsUI.SetActive(false);
+        UpgradesUI.SetActive(false);
+        GameplayUI.SetActive(false);
+        RunWinUI.SetActive(false);
+        RunEndUI.SetActive(false);
+    }
+
+    public void UIGameplay()
+    {
+        MainMenuUI.SetActive(false);
+        PauseUI.SetActive(false);
+        GameplayUI.SetActive(true);
+        SettingsUI.SetActive(false);
+        CreditsUI.SetActive(false);
+        UpgradesUI.SetActive(false);
+        RunWinUI.SetActive(false);
+        RunEndUI.SetActive(false);
+    }
+
+    public void UISettings()
+    {
+        MainMenuUI.SetActive(false);
+        PauseUI.SetActive(false);
+        GameplayUI.SetActive(false);
+        SettingsUI.SetActive(true);
+        CreditsUI.SetActive(false);
+        UpgradesUI.SetActive(false);
+        RunWinUI.SetActive(false);
+        RunEndUI.SetActive(false);
+    }
+
+    public void UIRunEnd()
+    {
+        MainMenuUI.SetActive(false);
+        PauseUI.SetActive(false);
+        GameplayUI.SetActive(false);
+        SettingsUI.SetActive(false);
+        CreditsUI.SetActive(false);
+        UpgradesUI.SetActive(false);
+        RunEndUI.SetActive(true);
+        RunWinUI.SetActive(false);
+    }
+
+    public void UICredits()
+    {
+        MainMenuUI.SetActive(false);
+        PauseUI.SetActive(false);
+        GameplayUI.SetActive(false);
+        SettingsUI.SetActive(false);
+        CreditsUI.SetActive(true);
+        UpgradesUI.SetActive(false);
+        RunWinUI.SetActive(false);
+        RunEndUI.SetActive(false);
+    }
+
+    public void UIRunWin()
+    {
+        MainMenuUI.SetActive(false);
+        PauseUI.SetActive(false);
+        GameplayUI.SetActive(false);
+        SettingsUI.SetActive(false);
+        CreditsUI.SetActive(false);
+        UpgradesUI.SetActive(false);
+        RunWinUI.SetActive(true);
+        RunEndUI.SetActive(false);
+    }
+
+    public void UIUpgrades()
+    {
+        MainMenuUI.SetActive(false);
+        PauseUI.SetActive(false);
+        GameplayUI.SetActive(false);
+        SettingsUI.SetActive(false);
+        CreditsUI.SetActive(false);
+        UpgradesUI.SetActive(true);
+        RunWinUI.SetActive(false);
+        RunEndUI.SetActive(false);
     }
 }

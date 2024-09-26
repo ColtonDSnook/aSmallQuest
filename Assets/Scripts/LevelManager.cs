284063bb89956be8be565c9b62d2380a1d93f7bf
Charlie <charlieryancurry@gmail.com>
2024-09-26 11:09:34 -0300

    add levelManager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,18 +1,122 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    GameManager gameManager;
+    public string previousScene;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadScene(string levelName)
+    {
+        previousScene = SceneManager.GetActiveScene().name;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        if (levelName.StartsWith("Gameplay"))
+        {
+            gameManager.gameState = GameManager.GameState.Gameplay;
+        }
+        //if (levelName == "Gameplay_Town1")
+        //{
+        //    foreach (GameObject gameObject in gameManager.level1Interactables)
+        //    {
+        //        gameObject.SetActive(true);
+        //    }
+        //    foreach (GameObject gameObject in gameManager.level2Interactables)
+        //    {
+        //        gameObject.SetActive(false);
+        //    }
+        //}
+        //if (!levelName.StartsWith("Gameplay"))
+        //{
+        //    foreach (GameObject gameObject in gameManager.level1Interactables)
+        //    {
+        //        gameObject.SetActive(false);
+        //    }
+        //    foreach (GameObject gameObject in gameManager.level2Interactables)
+        //    {
+        //        gameObject.SetActive(false);
+        //    }
+        //}
+        //if (levelName == "Gameplay_Town2")
+        //{
+        //    foreach (GameObject gameObject in gameManager.level1Interactables)
+        //    {
+        //        gameObject.SetActive(false);
+        //    }
+        //    foreach (GameObject gameObject in gameManager.level2Interactables)
+        //    {
+        //        gameObject.SetActive(true);
+        //    }
+        //}
+        //foreach (InteractableObject item in gameManager.interactables)
+        //{
+        //    if (item.scene == levelName && item.active)
+        //    {
+        //        item.gameObject.SetActive(true);
+        //    }
+        //    else
+        //    {
+        //        item.gameObject.SetActive(false);
+        //    }
+        //}
+        if (levelName == "Settings")
+        {
+            gameManager.gameState = GameManager.GameState.Settings;
+        }
+        if (levelName == "Main Menu")
+        {
+            Time.timeScale = 1;
+            gameManager.gameState = GameManager.GameState.MainMenu;
+        }
+        if (levelName == "RunEnd")
+        {
+            gameManager.gameState = GameManager.GameState.RunEnd;
+        }
+        SceneManager.LoadScene(levelName);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (gameManager.playerSprite.activeSelf == true)
+        {
+            gameManager.spawnPoint = GameObject.FindWithTag("SpawnPoint");
+            gameManager.MovePlayerToSpawnPoint();
+        }
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void LoadPreviousScene()
+    {
+        SceneManager.sceneLoaded += OnPreviousSceneLoaded;
+        if (previousScene.StartsWith("Gameplay"))
+        {
+            gameManager.gameState = GameManager.GameState.Gameplay;
+            Time.timeScale = 1;
+        }
+        if (previousScene == "MainMenu")
+        {
+            gameManager.gameState = GameManager.GameState.MainMenu;
+        }
+        SceneManager.LoadScene(previousScene);
+    }
+
+    private void OnPreviousSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        //gameManager.MovePlayerToPreviousLocation();
+        SceneManager.sceneLoaded -= OnPreviousSceneLoaded;
     }
 }

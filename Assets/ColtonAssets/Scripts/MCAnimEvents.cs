639881c04aaa16e6ee216275871a505bf8e18068
ColtonDSnook <144720034+ColtonDSnook@users.noreply.github.com>
2024-10-22 15:34:49 -0300

    Added spin and slash events for character

---
+++ Assets/ColtonAssets/Scripts/MCAnimEvents.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCAnimEvents : MonoBehaviour
+{
+    public ParticleSystem spinSlash;
+    public ParticleSystem slash;
+    public ParticleSystem stabAttack;
+
+    void MCAttackEvent()
+    {
+        slash.Play();
+    }
+    void MCSpinEvent()
+    {
+        spinSlash.Play();
+    }
+}

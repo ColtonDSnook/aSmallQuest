cae9f52aa74122939217671084ba7dbb441cbd78
ColtonDSnook <144720034+ColtonDSnook@users.noreply.github.com>
2024-10-22 15:06:32 -0300

    Finished Goblin event

--- Assets/ColtonAssets/Scripts/SlimeAttack.cs
+++ Assets/ColtonAssets/Scripts/SlimeAttack.cs
@@ -10,15 +10,4 @@ public class SlimeAttack : MonoBehaviour
     {
         slimeAttack.Play();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
